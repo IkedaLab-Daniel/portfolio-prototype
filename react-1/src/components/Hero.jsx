2c410ed77b4d92908d7719925bf4a6749e54e1ab
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-05 01:37:14 +0800

    Initial react version. Add navbar

---
+++ react-1/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar"
+const Hero = () =>{
+
+    return(
+        <>
+            <div className="hero">
+                <Navbar />
+                Hero
+            </div>
+        </>
+    )
+}
+
+export default Hero
---
+++ react-1/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () =>{
+
+    return(
+        <div className="navbar">
+            <div className="navbar-wrapper">
+                <span>Home</span>
+                <span>About</span>
+                <span>Skills</span>
+                <span>Project</span>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
